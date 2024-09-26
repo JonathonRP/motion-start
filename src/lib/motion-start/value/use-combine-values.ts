8ea788497fed87e722dcc7031779a8bb3a776393
JonathonRP <jreesep@gmail.com>
2024-09-26 20:42:08 +0000

    update type to be more accurate

--- src/lib/motion-start/value/use-combine-values.ts
+++ src/lib/motion-start/value/use-combine-values.ts
@@ -2,7 +2,7 @@
 based on framer-motion@4.1.17,
 Copyright (c) 2018 Framer B.V.
 */
-import { MotionValue } from ".";
+import type { MotionValue } from '.';
 
 /** 
 based on framer-motion@4.0.3,
@@ -12,42 +12,42 @@ import sync from 'framesync';
 import { motionValue } from '.';
 
 export const useCombineMotionValues = <R>(values: MotionValue[], combineValues: () => R) => {
+	let subscriptions: (() => void)[] = [];
+	let vals = values;
 
-  let subscriptions: Function[] = <any>[];
-  let vals = values;
+	const unsubscribe = () => {
+		for (const unsubscribe of subscriptions) {
+			unsubscribe();
+		}
+	};
+	const subscribe = () => {
+		subscriptions = vals.map((val) => val.onChange(handler));
+		updateValue();
+	};
+	const value = motionValue(combineValues(), () => {
+		unsubscribe();
+		subscribe();
+		return unsubscribe;
+	}) as MotionValue<R> & { reset: (values: MotionValue[], combineValues: () => R) => void };
 
-  
-  const unsubscribe = ()=>{
-    subscriptions.forEach((unsubscribe) => unsubscribe())
-  }
-  const subscribe = () => {
-    subscriptions = vals.map((val) => val.onChange(handler))
-    updateValue();
-  }
-  const value = motionValue(combineValues(), () => {
-    unsubscribe()
-    subscribe()
-    return unsubscribe;
-  }) as MotionValue<R> & { reset: ( values:MotionValue[], combineValues:()=>R ) => void };
+	let updateValue = () => {
+		value.set(combineValues());
+	};
 
-  let updateValue = () => {
-    value.set(combineValues());
-  }
+	const handler = () => {
+		sync.update(updateValue, false, true);
+	};
 
-  const handler = () => {
-    sync.update(updateValue, false, true);
-  }
+	value.reset = (_values, _combineValues) => {
+		vals = _values;
+		//cleanup and reset
+		unsubscribe();
+		updateValue = () => {
+			value.set(_combineValues());
+		};
+		subscribe();
+	};
 
-  value.reset = (_values, _combineValues) => {
-    vals=_values;
-    //cleanup and reset
-    unsubscribe()
-    updateValue = () => {
-      value.set(_combineValues())
-    }
-    subscribe()
-  }
-
-  return value;
-}
+	return value;
+};
 // export { default as UseCombineMotionValues } from "./UseCombineValues.svelte";
