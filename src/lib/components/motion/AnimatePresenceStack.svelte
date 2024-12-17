5714f6815855f7b3da294ab204e37d7475279480
Cliff Njoroge <37869706+cliffordkleinsr@users.noreply.github.com>
2024-12-17 11:34:52 +0300

    Restore Legacy animatepresence stack with conditional ✅

--- src/lib/components/motion/AnimatePresenceStack.svelte
+++ src/lib/components/motion/AnimatePresenceStack.svelte
@@ -1,11 +1,29 @@
 <script>
     import Box from "../Box.svelte";
-    import { AnimatePresence } from "$lib/motion-start";
+    import { Label } from "$lib/components/ui/label";
+    import { Switch } from "$lib/components/ui/switch";
+    import { AnimatePresence, AnimatePresenceLegacy } from "$lib/motion-start";
     import Card from "$lib/components/Card.svelte";
     let index = 0;
+    let use_legacy= false
     $: mint = index + 1;
 </script>
-
+<div class="flex items-center space-x-2">
+    <Switch id="airplane-mode" bind:checked={use_legacy}/>
+    <Label for="airplane-mode">Use Legacy</Label>
+  </div>
+{#if use_legacy}
+<Box>
+    <div
+        class="w-64 h-64 relative bg-gray-700/40 rounded-lg flex justify-center items-center"
+    >
+        <AnimatePresenceLegacy initial={false} list={[{ key: index }]}>
+            <Card bind:index={mint} frontCard={false} />
+            <Card bind:index drag="x" frontCard={true} />
+        </AnimatePresenceLegacy>
+    </div>
+</Box>
+{:else}
 <Box>
     <div
         class="w-64 h-64 relative bg-gray-700/40 rounded-lg flex justify-center items-center"
@@ -16,3 +34,4 @@
         </AnimatePresence>
     </div>
 </Box>
+{/if}
--- src/lib/motion-start/components/AnimatePresence/index.ts
+++ src/lib/motion-start/components/AnimatePresence/index.ts
@@ -43,4 +43,5 @@ export type { AnimatePresenceProps } from "./types.js";
  */
 export type ConditionalGeneric<T> = T extends {key:any} ? T : { key: 1}; // Better handling of defaults and the optional list prop
 export { default as AnimatePresence } from './AnimatePresence.svelte';
+export { default as AnimatePresenceLegacy } from './AnimatePresenceLegacy.svelte';
 export { PresenceChild } from './PresenceChild/index.js';
