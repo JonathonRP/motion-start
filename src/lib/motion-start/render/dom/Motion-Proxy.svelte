9e4b5cfcfdb9f1b3b23019c28b8743a8c41d24c8
Jonathon Reese Perry <jreesep@gmail.com>
2025-09-29 00:05:03 +0000

    fix: 🐛 motion svg
    fix motion svg
    
    BREAKING CHANGE: 🧨 none

--- src/lib/motion-start/render/dom/Motion-Proxy.svelte
+++ src/lib/motion-start/render/dom/Motion-Proxy.svelte
@@ -1,25 +1,31 @@
 <!-- <svelte:options runes={true}/> -->
 <script lang="ts">
-    import type { SvelteHTMLElements } from "svelte/elements";
-    import type { MotionProps } from "../../motion";
-    import Motion from "../../motion/Motion.svelte";
+  import type { SvelteHTMLElements } from "svelte/elements";
+  import type { MotionProps } from "../../motion";
+  import Motion from "../../motion/Motion.svelte";
 
-    // let {as, class: className, children, ...restProps}: {as: keyof SvelteHTMLElements, children: Snippet, class: string } & MotionProps = $props();
-    export let ___tag: keyof SvelteHTMLElements;
-    export let el: SvelteHTMLElements[typeof ___tag]["this"];
+  // let {as, class: className, children, ...restProps}: {as: keyof SvelteHTMLElements, children: Snippet, class: string } & MotionProps = $props();
+  export let ___tag: keyof SvelteHTMLElements;
+  export let el: SvelteHTMLElements[typeof ___tag]["this"];
+  export let isSVG = false;
 </script>
 
-<Motion {...$$restProps} let:props let:motion>
-    <svelte:element
-        this={___tag}
-        {...Object.fromEntries(Object.entries(props).filter(
-            ([key, _]) => key !== "latestvalues" && key !== "renderstate",
-        ))}
-        bind:this={el}
-        class={props.class}
-        xmlns={isSVG ? "http://www.w3.org/2000/svg" : undefined}
-        use:motion
-    >
-        <slot />
-    </svelte:element>
+<Motion {...$$restProps} let:props let:motion {isSVG}>
+  <svelte:element
+    this={___tag}
+    {...Object.fromEntries(
+      Object.entries(props).filter(
+        ([key, _]) =>
+          key !== "latestvalues" &&
+          key !== "renderstate" &&
+          key !== "visualProps"
+      )
+    )}
+    bind:this={el}
+    class={props.class}
+    xmlns={isSVG ? "http://www.w3.org/2000/svg" : undefined}
+    use:motion
+  >
+    <slot />
+  </svelte:element>
 </Motion>
