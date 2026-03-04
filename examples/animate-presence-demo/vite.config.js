49881b7f2f5c90b68f3f2680b18dd7a11b25f714
Jonathon Reese Perry <jreesep@gmail.com>
2026-03-04 16:21:05 +0000

    fix: add Vite optimizeDeps for StackBlitz compatibility

--- examples/animate-presence-demo/vite.config.js
+++ examples/animate-presence-demo/vite.config.js
@@ -3,4 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    include: ['motion-start', 'framesync', 'popmotion', 'style-value-types']
+  }
 })
