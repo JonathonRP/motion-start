b8b8d71a8650ace0a595ce422a2279ebaa4da970
Jonathon Reese Perry <jreesep@gmail.com>
2026-03-13 01:15:48 +0000

    chore: remove animate-presence-demo and card-stack examples, along with presence-affects-layout example files

--- examples/card-stack/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-    include: ['motion-start', 'framesync', 'popmotion', 'style-value-types']
-  }
-})
